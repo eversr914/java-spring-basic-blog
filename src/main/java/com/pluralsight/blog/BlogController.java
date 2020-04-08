58602c721d97dffcb6f87540416db3d6896f2b0b
Robert Evers <robert.evers9@gmail.com>
2020-04-08 11:48:24 -0700

    Update BlogController.java

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -6,8 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class BlogController {
     @RequestMapping("/")
-    public String listPosts(ModelMap map){
-        map.put("title", "Blog Post 1");
+    public String listPosts(){
         return "home";
     }
 }
