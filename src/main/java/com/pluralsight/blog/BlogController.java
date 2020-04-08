b16207544c79701b53da3f8ef5f917dbd884264b
Robert Evers <robert.evers9@gmail.com>
2020-04-08 11:52:47 -0700

    Module 1a

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -2,11 +2,13 @@ package com.pluralsight.blog;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.ModelMap;
 
 @Controller
 public class BlogController {
     @RequestMapping("/")
-    public String listPosts(){
+    public String listPosts(ModelMap modelMap){
+        modelMap.put("title", "Blog Post 1");
         return "home";
     }
 }
